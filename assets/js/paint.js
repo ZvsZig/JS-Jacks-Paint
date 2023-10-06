000daae135f9c6f7415e2c2692b680fa8af53198
zig <zvszig@gmail.com>
2023-10-06 12:32:29 -0700

    finished product

---
+++ assets/js/paint.js
@@ -0,0 +1,71 @@
+function configureListeners() {
+    let images = // select img elements  
+
+
+     for (var i = 0; i < images.length; i++) {        
+        // iterate over images and add mouseover event listeners      
+    } 
+}
+
+function addOpacity(event) {
+    // add appropriate CSS class
+    getProductInfo(event.target.id);     
+}
+
+function removeOpacity(event) {
+     //remove appropriate CSS class
+
+    let element = document.getElementById('color-price');
+        element.textContent = '';
+        
+    let color = document.getElementById('color-name');
+        color.textContent = ''; 
+
+    event.preventDefault();    
+}
+
+function getProductInfo(paintColor) {
+    let price;
+    let colorName;  
+    
+    switch (paintColor) {
+        case 'pn1':           
+            // set variables for price and color name and invoke a function to update the price     
+            break;           
+        case 'pn2':
+            // set variables for price and color name and invoke a function to update the price    
+            break;            
+        case 'pn3':
+            // set variables for price and color name and invoke a function to update the price  
+            break;   
+        case 'pn4':
+            // set variables for price and color name and invoke a function to update the price  
+            break;   
+        case 'pn5':
+            // set variables for price and color name and invoke a function to update the price       
+            break;   
+        case 'pn6':
+            // set variables for price and color name and invoke a function to update the price        
+            break;   
+        case 'pn7':
+            // set variables for price and color name and invoke a function to update the price 
+            break;   
+        case 'pn8':
+            // set variables for price and color name and invoke a function to update the price   
+            break;   
+        case 'pn9':
+            // set variables for price and color name and invoke a function to update the price 
+            break;   
+          default:              
+    }
+
+    function updatePrice(colorName, price)
+    {       
+        let colorPrice = // select element with corresponding id
+        // display price
+        
+        let color = // select element with corresponding id
+        //display color name
+    }
+    
+}
